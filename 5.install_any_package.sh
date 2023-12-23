9dfe39445cbb19fcd249a7f33c0d4a768b2f4590
das-shiv <ssdas.in@gmail.com>
2023-12-23 08:49:38 +0000

    Updated exit code scripts

--- 5.install_any_package.sh
+++ 5.install_any_package.sh
@@ -4,12 +4,14 @@
 
 read -p "Enter the name of the package you want to install: " package
 
-sudo apt update && sudo apt install $package -y
+sudo apt update >> /dev/null && sudo apt install $package -y >> /dev/null
 
 if [ $? -eq 0 ]
 then
 	echo "$package installed successfully, the $package is available at: "
 	which $package
+	exit 0  # If the condition is true it exit with exit code 0
 else
 	echo "$package was not installed, make sure you have entered a valid name."
+	exit 5 # If the package was not installed we get a exit code 5
 fi
--- 6.uninstall_any_package.sh
+++ 6.uninstall_any_package.sh
@@ -4,11 +4,13 @@
 
 read -p "Enter the name of the package you want to install: " package
 
-sudo apt purge $package -y
+sudo apt purge $package -y >> /dev/null
 
 if [ $? -eq 0 ]
 then
 	echo "$package uninstalled successfully"
+	exit 46 #exit code is set to 46 if package was uninstalled
 else
 	echo "$package was not uninstalled, make sure you have entered a valid name."
+	exit 120 #exit code is set to 120 if the package was not installed
 fi
